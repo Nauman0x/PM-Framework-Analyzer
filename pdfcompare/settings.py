b469b16d75b354f2d47bd294e41b8626b71e2732
ashna <149528794+ashnamasood@users.noreply.github.com>
2025-10-05 23:12:51 +0500

    Update settings.py
    fixed

--- pdfcompare/settings.py
+++ pdfcompare/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'replace-me-in-production')
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
-ALLOWED_HOSTS='pm-framework-analyzer-feg2as3nb-ashnamasoods-projects.vercel.app'
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
